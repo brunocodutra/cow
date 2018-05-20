93babc5df64b7942c99c3c54b9cbee91a27f0811
Bruno Dutra <brunocodutra@gmail.com>
2018-05-20 18:05:26 +0200

    move is_convertible to detail/traits.hpp

---
+++ include/cow/detail/traits.hpp
@@ -0,0 +1,29 @@
+#ifndef COW_DETAIL_TRAITS_HPP
+#define COW_DETAIL_TRAITS_HPP
+
+#include <type_traits>
+
+namespace cow::detail {
+    template<typename to>
+    void convert(to);
+
+    template<typename To,
+        typename... From,
+        typename = decltype(convert<To>({std::declval<From>()...})),
+        bool disabled = std::is_array_v<To> || std::is_function_v<To>>
+    std::true_type is_convertible_impl(std::enable_if_t<!disabled, int>);
+
+    template<typename, typename...>
+    std::false_type is_convertible_impl(...);
+
+    template<typename To, typename... From>
+    struct is_convertible : decltype(is_convertible_impl<To, From...>(0)) {};
+
+    template<typename To, typename From>
+    struct is_convertible<To, From> : std::is_convertible<From, To> {};
+
+    template<typename To, typename... From>
+    constexpr bool is_convertible_v = is_convertible<To, From...>::value;
+}
+
+#endif
--- include/cow/value.hpp
+++ include/cow/value.hpp
@@ -1,6 +1,8 @@
 #ifndef COW_VALUE_HPP
 #define COW_VALUE_HPP
 
+#include "detail/traits.hpp"
+
 #include <cassert>
 #include <functional>
 #include <memory>
@@ -8,28 +10,6 @@
 #include <utility>
 
 namespace cow::detail {
-    template<typename to>
-    void convert(to);
-
-    template<typename To,
-        typename... From,
-        typename = decltype(convert<To>({std::declval<From>()...})),
-        bool disabled = std::is_array_v<To> || std::is_function_v<To>>
-    std::true_type is_convertible_impl(std::enable_if_t<!disabled, int>);
-
-    template<typename, typename...>
-    std::false_type is_convertible_impl(...);
-
-    template<typename To, typename... From>
-    struct is_convertible : decltype(is_convertible_impl<To, From...>(0)) {};
-
-    template<typename To, typename From>
-    struct is_convertible<To, From> : std::is_convertible<From, To> {};
-
-    template<typename To, typename... From>
-    constexpr bool is_convertible_v = is_convertible<To, From...>::value;
-
-    static_assert(is_convertible_v<int>);
 
     template<typename T,
         bool move = is_convertible_v<T, std::decay_t<T>&&>,
